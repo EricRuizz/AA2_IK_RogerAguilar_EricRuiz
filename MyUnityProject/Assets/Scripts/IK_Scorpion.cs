a7973553e0bec7f26409ecc5774e073f04498ebc
Eric <eric.ruiz@enti.cat>
2022-12-13 12:44:16 +0100

    scorpion legs fixed

--- MyUnityProject/Assets/Scripts/IK_Scorpion.cs
+++ MyUnityProject/Assets/Scripts/IK_Scorpion.cs
@@ -49,27 +49,31 @@ public class IK_Scorpion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(animPlaying)
-            animTime += Time.deltaTime;
+        //if (animPlaying)
+        //    animTime += Time.deltaTime;
 
         NotifyTailTarget();
-        
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            NotifyStartWalk();
-            animTime = 0;
-            animPlaying = true;
-        }
 
-        if (animTime < animDuration)
-        {
-            Body.position = Vector3.Lerp(StartPos.position, EndPos.position, animTime / animDuration);
-        }
-        else if (animTime >= animDuration && animPlaying)
-        {
-            Body.position = EndPos.position;
-            animPlaying = false;
-        }
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    NotifyStartWalk();
+        //    animTime = 0;
+        //    animPlaying = true;
+        //}
+
+        //if (animTime < animDuration)
+        //{
+        //    Body.position = Vector3.Lerp(StartPos.position, EndPos.position, animTime / animDuration);
+        //}
+        //else if (animTime >= animDuration && animPlaying)
+        //{
+        //    Body.position = EndPos.position;
+        //    animPlaying = false;
+        //}
+
+        NotifyStartWalk();
+        animTime = 0;
+        animPlaying = true;
 
         ////////
         UpdateFutureLegBases();
@@ -114,10 +118,9 @@ public class IK_Scorpion : MonoBehaviour
         {
             RaycastHit hit;
 
-            if (Physics.Raycast(raycastFutureLegBases[i].position, Vector3.down, out hit, 10.0f))
+            if (Physics.Raycast(raycastFutureLegBases[i].position, Vector3.down, out hit, 2.0f))
             {
                 futureLegBases[i].transform.position = hit.point;
-                Debug.Log(hit.transform.name);
             }
         }
     }
@@ -129,11 +132,14 @@ public class IK_Scorpion : MonoBehaviour
 
         Vector3 bPos = Body.transform.position;
         Body.transform.position = new Vector3(bPos.x, bodyInitHeight + heightDiff, bPos.z);
-        
+    }
+
+    private void OnDrawGizmos()
+    {
         for (int i = 0; i < futureLegBases.Length; i++)
         {
-            Vector3 fPos = futureLegBases[i].transform.position;
-            futureLegBases[i].transform.position = new Vector3(fPos.x, futureLegBases[i].transform.position.y - heightDiff, fPos.z);
+            Gizmos.DrawSphere(futureLegBases[i].transform.position, 0.1f);
+
         }
     }
 }
