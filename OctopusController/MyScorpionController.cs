ecc12df943eeb785ee8720ff17f317d9de14d570
Eric <eric.ruiz@enti.cat>
2023-01-13 20:18:16 +0100

    tried to add rule

--- OctopusController/MyScorpionController.cs
+++ OctopusController/MyScorpionController.cs
@@ -293,7 +293,7 @@ namespace OctopusController
             {
                 float gradient = CalculateGradient(target, tailSolutions, i, learningRate);
                 //tailSolutions[i] = tailSolutions[i] - gradient;
-                tailSolutions[i] -= 100.0f * shootingStrength * gradient + NewErrorFunction(i) / 20.0f; //TOASK - aplicar el NewErrorFunction() amb una suma a gradient?
+                tailSolutions[i] -= 100.0f * shootingStrength * gradient + NewErrorFunction(i) / 20.0f;
                 _tail.Bones[i].localRotation = Quaternion.Euler(tailSolutions[i] * axis[i]);
                 //_tail.Bones[i].Rotate(axis[i] * tailSolutions[i]);
 
@@ -339,7 +339,8 @@ namespace OctopusController
 
         private float NewErrorFunction(int i)
         {
-            return Vector3.Dot(new Vector3(0.0f, -1.0f, 0.0f), _tail.Bones[i].forward);
+            //return Vector3.Dot(new Vector3(0.0f, -1.0f, 0.0f), _tail.Bones[i].forward);
+            return Vector3.Dot(new Vector3(0.0f, -1.0f, 0.0f), _tail.Bones[_tail.Bones.Length - 1].forward - _tail.Bones[_tail.Bones.Length - 2].forward);
         }
 
         #endregion
